1033c545e197b286bccb09946ea534d09c162cbd
Isadora Veiga <91499590+IsaBorges@users.noreply.github.com>
2024-01-14 13:45:21 -0300

    Update funcao_orgao_judicializado.py

--- funcao_orgao_judicializado.py
+++ funcao_orgao_judicializado.py
@@ -3,9 +3,5 @@ from funcao_arrumar import arrumar
 def orgao_judicializado(celula):
     print("Função orgao_judicializado: Em andamento")
     arrumar(celula)
-    #if celula.text[-1] != ')':
-      #celula.text += ')'
-    #else:
-      #pass
     print("Função orgao_judicializado: Finalizada")
     return celula.text
