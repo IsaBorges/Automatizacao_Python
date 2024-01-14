0bfe0f5f8f849dc2f2dc63b608a853737cfcb21e
Isadora Veiga <91499590+IsaBorges@users.noreply.github.com>
2024-01-14 13:44:23 -0300

    Update funcao_doc_comunicacao_judicializacao.py

--- funcao_doc_comunicacao_judicializacao.py
+++ funcao_doc_comunicacao_judicializacao.py
@@ -3,7 +3,7 @@ import re
 
 def doc_comunicacao_judicializacao(celula):
     print("Função doc_comunicacao_judicializacao: Em andamento")
-    #Regex para ignorar o que vem entre parenteses, que normamelmente é o número SEI do documento
+    #Regex para ignorar o que vem entre parenteses
     pdr_documento = re.sub(r'\([^)]*\)', '', celula.text)
     celula.text = pdr_documento
     arrumar(celula)
